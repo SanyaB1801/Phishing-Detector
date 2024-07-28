621b8a34bad9156e15ee4568d79060cdba2094c3
Sanya Behera <64143038+SanyaB1801@users.noreply.github.com>
2024-07-28 12:35:34 +0530

    Update nltk_data.py

--- nltk_data.py
+++ nltk_data.py
@@ -1,6 +1,6 @@
 import nltk
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+nltk.download('punkt', download_dir = 'content/nltk data/')
+nltk.download('stopwords', download_dir = 'content/nltk data/')
+nltk.download('wordnet', download_dir = 'content/nltk data/')
+nltk.download('omw-1.4', download_dir = 'content/nltk data/')
